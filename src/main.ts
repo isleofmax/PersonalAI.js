738b14d8c36a9cd0be4fffb55b75c8c0943b0add
Massimo <maxpucci78@gmail.com>
2026-07-12 10:11:26 +0200

    Added argv parse

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
+import { argv } from "node:process";
 import { repl } from "./repl.js";
 
 function main() {
-	repl();
+	parse_argv();
+}
+
+function parse_argv() {
+	const command = argv[0].toLocaleLowerCase();
+	if (command === undefined) {
+		console.log("Usage personalai <command>:")
+		print_help();
+		return;
+	}
+
+	switch(command) {
+		case "help":
+			print_help();
+			break;
+		case "cli":
+			repl();
+			break;
+		default:
+			console.log("Unknown command");
+			print_help();			
+	}	
+}
+
+function print_help() {
+
 }
 
 main();
